2ce0d6e468777951a4ee917305579b4288c22809
Jimwell040 <jimwellibay040@gmail.com>
2022-04-03 18:18:48 +0800

    Second update of cellphone site

--- public/pages/mobile/script/main.js
+++ public/pages/mobile/script/main.js
@@ -13,11 +13,46 @@ window.setInterval(() => {
 // Main program
 let btnMenu = $("#btnMenu");
 let menu = $("#menu");
+let message1 = $("#message1");
+let message2 = $("#message2");
+let message3 = $("#message3");
 
-btnMenu.on("click", () => {
+$(document).ready(() => {
   
-  menu.toggle("slow");
+  btnMenu.on("click", () => {
   
-  btnMenu.toggleClass("btnAdjust btnDefault");
+    menu.toggle("slow");
+  
+    btnMenu.toggleClass("btnAdjust btnDefault");
+  
+  });
+  
+  message1.click(() => {
+  
+    messageManipulate(message1, " inverted");
+  
+  });
+  
+  message2.click(() => {
+  
+    messageManipulate(message2, " inverted");
+  
+  });
+  
+  message3.click(() => {
+  
+    messageManipulate(message3, " inverted");
+  
+  });
   
 });
+
+const messageManipulate = (id,add) => {
+  
+  let clas = id.attr("class");
+  
+  id.removeClass();
+  id.toggleClass(clas + add);
+  
+}
+
